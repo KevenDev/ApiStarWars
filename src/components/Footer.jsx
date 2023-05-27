36d3e38c8b8f96ca534567d0900d29745ea2147b
Keven Teles <glevenkeven@gmail.com>
2023-05-27 16:34:05 -0300

    feat: novo componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import LogoStarWars from "../assets/logo.png"
+import sabreIcon from "../assets/sabreIcon.png"
 
 const Navbar = ()=> {
   return (
-    <div>Navbar</div>
+    <>
+    <div className='bg-black w-screen absolute bottom-0 h-20 flex justify-center items-center'>
+      <img className='h-16 text-yellow-400' src={sabreIcon} alt="" />
+    </div>
+    </>
+    
   )
 }
 
