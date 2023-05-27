8923451aafbb260fd18ebfdfc62066415274f5c1
Keven Teles <glevenkeven@gmail.com>
2023-05-27 16:34:46 -0300

    feat: componente navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useState } from "react";
+import LogoStarWars from "../assets/logo.png"
 
-const Navbar = ()=> {
+
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    <div className="h-20 bg-black flex justify-center">
+        <img className="h-full" src={LogoStarWars} alt="" />
+    </div>
+  );
+};
+export default Navbar;
