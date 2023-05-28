f7ed46d2a46ccdf7638c7b92cc497e6790b93f17
Keven Teles <glevenkeven@gmail.com>
2023-05-28 01:30:49 -0300

    feat: paginação adicionada com pagination do ant design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import star from '../assets/starwars.jpg'
 import yoda from '../assets/yoda.svg'
-import { Pagination } from 'antd';
+import {  Pagination  } from 'antd';
 
 const Hero = ()=> {
 
   const [names, setNames] = useState([])
-  const [activeIndex, setActiveIndex] = useState(1)
+  const [current, setCurrent] = useState(1)
+
+  const onChange = (page) => {
+    setCurrent(page);
+  };
 
   useEffect(() => {
-    fetch(`https://swapi.dev/api/people/?page=${activeIndex}`)
+    fetch(`https://swapi.dev/api/people/?page=${current}`)
       .then((res) => res.json())
       .then((response) => {
         setNames([response]);
         console.log(response);
       });
-  }, [activeIndex]);
+  }, [current]);
   
 
   return (
@@ -26,32 +30,25 @@ const Hero = ()=> {
         style={{ backgroundImage: `url(${star})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
       />
        <div className="absolute w-full h-[calc(100vh-160px)] bg-black opacity-80"></div> 
-      <div className="relative z-10 p-4 grid md:grid-cols-4 grid-cols-2  gap-2 place-items-center">
+      <div className="relative z-10 p-4 grid md:grid-cols-4 grid-cols-2 gap-2 place-items-center">
         {names.length > 0 &&
           names[0].results.map((item) => (
             <div
               key={item.name}
-              className="w-40 h-30 bg-black bg-opacity-30 p-4 text-center flex flex-col text-white"
+              className="md:w-50 md:h-[6.2rem] w-40 h-[4.3rem] bg-black bg-opacity-30 p-4 text-center flex flex-col text-white rounded-lg"
             >
-              <img className="w-10 h-10 mx-auto bg-opacity-30" src={yoda} alt="" />
-              <h2 className="text-xl">Name: {item.name}</h2>
+              <img className="xl:h-20 w-10 h-10 mx-auto bg-opacity-30" src={yoda} alt="" />
+              <h2 className="text-sm">{item.name}</h2>
             </div>
           ))}
       </div>
-      <div className="relative mt-4 px-4 flex justify-center">
-      <Pagination
-      defaultCurrent={6}
-      total={500}
-      showSizeChanger={false}
-      responsive={true}
-      itemRender={(current, type, originalElement) =>
-        type === 'page' ? (
-          <div className="bg-black bg-opacity-80 text-white px-3  rounded">{current}</div>
-        ) : (
-          originalElement
-        )
-      }
-    />
+      <div className="relative flex justify-center">
+        <Pagination current={current} onChange={onChange} total={90} className="bg-black bg-opacity-40" showSizeChanger={false} itemRender={(page, type, originalElement) => {
+        if (type === 'page') {
+          return <div className=" bg-black  text-white rounded">{page}</div>;
+        }
+        return originalElement;
+      }}/>;
       </div>
     </div>
   </>
